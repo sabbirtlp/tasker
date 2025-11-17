d4a7ceffbc769e71468ce79a9ddc76be3266faa2
sabbirtlp <sabbirtlp@gmail.com>
2025-11-18 00:02:51 +0600

    Just push code to the github

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Footer from "./components/Footer/Footer"
+import Header from "./components/Header/Header"
+import Hero from "./components/Hero/Hero"
+
+
+function App() {
+
+
+  return (
+    <>
+      <Header></Header>
+      <Hero></Hero>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
